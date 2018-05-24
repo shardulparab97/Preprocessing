e6ea737b4ce9c6ca9faf24732d20f85b7c8cccc4
shardulparab97 <shardulsvparab@gmail.com>
2018-05-24 12:06:14 +0530

    Added for grayscale - single channel

--- caffe/single_channel_preprocess.py
+++ caffe/single_channel_preprocess.py
@@ -1,33 +1,36 @@
+#code for preprocessing the image for caffe along with converting to grayscale/single channel
 import skimage
 import skimage.io as io
 import skimage.transform
+from skimage.color import rgb2gray
 import sys
 import numpy as np
 import math
+import matplotlib
 from matplotlib import pyplot
 import matplotlib.image as mpimg
 import scipy.misc
+import cv2
 
+#Taking image input
 IMAGE_LOCATION = "input_image.jpg"
 
 #Read Image
 img = skimage.img_as_float(skimage.io.imread(IMAGE_LOCATION)).astype(np.float32)
+print("Final shape is:",img.shape)
 
-#For converting to BGR
-imgBGR = img[:, :, (2, 1, 0)]
+#converting to grayscale
+img = rgb2gray(img)
+
 
 #For resizing
 imgResize = skimage.transform.resize(img, (128, 128))
 
-#For converting HWC to CHW Format
-imgCHW	 = imgResize.swapaxes(1, 2).swapaxes(0, 1)
+#adding the axis for channel
+imgResize = imgResize[np.newaxis, :, :].astype(np.float32)
 
+imgCHW = imgResize
 #Add axis of batches
 imgFinal = imgCHW[np.newaxis, :, :, :].astype(np.float32)
 
 print("Final shape is:",imgFinal.shape)
-
-scipy.misc.toimage(image_array, cmin=0.0, cmax=...).save('outfile.jpg')
-
-
-
